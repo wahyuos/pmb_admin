be13f13aec5f432199b70b0e2b1ce71e24bdd92c
wahyuos <wahyukamaludin@outlook.com>
2020-12-11 12:50:08 +0700

    penambahan fitur update data

--- system/application/models/M_pendaftar.php
+++ system/application/models/M_pendaftar.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-date_default_timezone_set("Asia/Jakarta");
+
 /**
  * @author     Wahyu Kamaludin
  * @category   Models
@@ -9,6 +9,7 @@ date_default_timezone_set("Asia/Jakarta");
 class M_pendaftar extends CI_Model
 {
 
+    // proses menyimpan data pendaftar
     public function create($data)
     {
         // value insert akun
@@ -273,19 +274,315 @@ class M_pendaftar extends CI_Model
         return $response;
     }
 
+    // ambil data lengkap si pendaftar dari view pada database berdasarkan ID
     public function read($id = null)
     {
         return $this->db->get_where('v_data_pd', ['id_pd' => $id])->row();
     }
 
-    public function update($data)
+    /**
+     * ============================================================
+     * BIODATA
+     * 
+     * 1. Proses ambil data biodata dari tabel biodata berdasarkan ID akun
+     * 2. Proses update data biodata berdasarkan ID akun
+     * ============================================================
+     */
+    public function get_biodata($id)
     {
+        $this->db->select('*')
+            ->from('pmb_biodata a')
+            ->join('ref_agama b', 'a.id_agama = b.id_agama', 'LEFT')
+            ->where(['id_akun' => $id]);
+        return $this->db->get()->row();
     }
 
-    public function delete($id)
+    public function edit_biodata($data)
     {
+        // set data untuk diupdate
+        $value = [
+            'nm_pd'      => strtoupper($data['nm_pd']),
+            'nik'        => $data['nik'],
+            'tmpt_lahir' => $data['tmpt_lahir'],
+            'tgl_lahir'  => $data['tgl_lahir'],
+            'id_agama'   => $data['id_agama'],
+            'jk'         => $data['jk'],
+            'updated_at' => date("Y-m-d H:i:s")
+        ];
+        // update tabel biodata
+        $update = $this->db->update('pmb_biodata', $value, ['id_akun' => $data['id_akun']]);
+        // cek status simpan
+        if ($update) {
+            // buat respon berhasil
+            $response = [
+                'status'  => true,
+                'message' => 'Biodata berhasil diubah',
+                'title'   => 'Berhasil!',
+                'type'    => 'success'
+            ];
+        } else {
+            // buat respon gagal
+            $response = [
+                'status'  => false,
+                'message' => 'Gagal merubah biodata',
+                'title'   => 'Gagal!',
+                'type'    => 'danger'
+            ];
+        }
+        return $response;
     }
 
+    /**
+     * ============================================================
+     * KONTAK
+     * 
+     * 1. Proses ambil data kontak dari tabel kontak berdasarkan ID akun
+     * 2. Proses update data kontak berdasarkan ID akun
+     * ============================================================
+     */
+    public function get_kontak($id)
+    {
+        return $this->db->get_where('pmb_kontak', ['id_akun' => $id])->row();
+    }
+
+    public function edit_kontak($data)
+    {
+        // set data untuk diupdate
+        $value = [
+            'no_hp'      => $data['no_hp'],
+            'no_hp_ortu' => $data['no_hp_ortu'],
+            'email'      => $data['email'],
+            'updated_at' => date("Y-m-d H:i:s")
+        ];
+        // update tabel biodata
+        $update = $this->db->update('pmb_kontak', $value, ['id_akun' => $data['id_akun']]);
+        // cek status simpan
+        if ($update) {
+            // buat respon berhasil
+            $response = [
+                'status'  => true,
+                'message' => 'Kontak berhasil diubah',
+                'title'   => 'Berhasil!',
+                'type'    => 'success'
+            ];
+        } else {
+            // buat respon gagal
+            $response = [
+                'status'  => false,
+                'message' => 'Gagal merubah kontak',
+                'title'   => 'Gagal!',
+                'type'    => 'danger'
+            ];
+        }
+        return $response;
+    }
+
+    /**
+     * ============================================================
+     * ALAMAT
+     * 
+     * 1. Proses ambil data alamat dari tabel alamat berdasarkan ID akun
+     * 2. Proses update data alamat berdasarkan ID akun
+     * ============================================================
+     */
+    public function get_alamat($id)
+    {
+        $this->db->select('*')
+            ->from('pmb_alamat a')
+            ->join('ref_wilayah b', 'a.id_wil = b.id_wil', 'LEFT')
+            ->where(['id_akun' => $id]);
+        return $this->db->get()->row();
+    }
+
+    public function edit_alamat($data)
+    {
+        // set data untuk diupdate
+        $value = [
+            'jln'      => $data['jln'],
+            'rt'       => $data['rt'],
+            'rw'       => $data['rw'],
+            'nm_dsn'   => $data['nm_dsn'],
+            'ds_kel'   => $data['ds_kel'],
+            'kode_pos' => $data['kode_pos'],
+            'id_prov'  => $data['id_prov'],
+            'id_kab'   => $data['id_kab'],
+            'id_wil'   => $data['id_wil'],
+        ];
+        // update tabel biodata
+        $update = $this->db->update('pmb_alamat', $value, ['id_akun' => $data['id_akun']]);
+        // cek status simpan
+        if ($update) {
+            // buat respon berhasil
+            $response = [
+                'status'  => true,
+                'message' => 'Alamat berhasil diubah',
+                'title'   => 'Berhasil!',
+                'type'    => 'success'
+            ];
+        } else {
+            // buat respon gagal
+            $response = [
+                'status'  => false,
+                'message' => 'Gagal merubah alamat',
+                'title'   => 'Gagal!',
+                'type'    => 'danger'
+            ];
+        }
+        return $response;
+    }
+
+    /**
+     * ============================================================
+     * ORANG TUA
+     * 
+     * 1. Proses ambil data ortu dari tabel ortu berdasarkan ID akun
+     * 2. Proses update data ortu berdasarkan ID akun
+     * ============================================================
+     */
+    public function get_ortu($id)
+    {
+        $this->db->select('*')
+            ->from('pmb_orang_tua a')
+            ->join('ref_penghasilan b', 'a.id_penghasilan_ayah = b.id_penghasilan', 'LEFT')
+            ->where(['id_akun' => $id]);
+        return $this->db->get()->row();
+    }
+
+    public function edit_ortu($data)
+    {
+        // set data untuk diupdate
+        $value = [
+            'nm_ayah'           => $data['nm_ayah'],
+            'id_pekerjaan_ayah' => $data['id_pekerjaan_ayah'],
+            'nm_ibu'            => $data['nm_ibu'],
+            'id_pekerjaan_ibu'  => $data['id_pekerjaan_ibu'],
+            'updated_at'        => date("Y-m-d H:i:s")
+        ];
+        // update tabel biodata
+        $update = $this->db->update('pmb_orang_tua', $value, ['id_akun' => $data['id_akun']]);
+        // cek status simpan
+        if ($update) {
+            // buat respon berhasil
+            $response = [
+                'status'  => true,
+                'message' => 'Orang tua berhasil diubah',
+                'title'   => 'Berhasil!',
+                'type'    => 'success'
+            ];
+        } else {
+            // buat respon gagal
+            $response = [
+                'status'  => false,
+                'message' => 'Gagal merubah ortu',
+                'title'   => 'Gagal!',
+                'type'    => 'danger'
+            ];
+        }
+        return $response;
+    }
+
+    /**
+     * ============================================================
+     * SEKOLAH ASAL
+     * 
+     * 1. Proses ambil data sekolah dari tabel sekolah berdasarkan ID akun
+     * 2. Proses update data sekolah berdasarkan ID akun
+     * ============================================================
+     */
+    public function get_sekolah_asal($id)
+    {
+        $this->db->select('*')
+            ->from('pmb_sekolah_asal a')
+            ->join('pmb_ref_masuk b', 'a.id_ref_masuk = b.id_ref_masuk', 'LEFT')
+            ->where(['id_akun' => $id]);
+        return $this->db->get()->row();
+    }
+
+    public function edit_sekolah_asal($data)
+    {
+        // set data untuk diupdate
+        $value = [
+            'jenjang'        => $data['jenjang'],
+            'sekolah'        => $data['sekolah'],
+            'alamat_sekolah' => $data['alamat_sekolah'],
+            'id_ref_masuk'   => $data['id_ref_masuk'],
+            'updated_at'     => date("Y-m-d H:i:s")
+        ];
+        // update tabel biodata
+        $update = $this->db->update('pmb_sekolah_asal', $value, ['id_akun' => $data['id_akun']]);
+        // cek status simpan
+        if ($update) {
+            // buat respon berhasil
+            $response = [
+                'status'  => true,
+                'message' => 'Sekolah asal berhasil diubah',
+                'title'   => 'Berhasil!',
+                'type'    => 'success'
+            ];
+        } else {
+            // buat respon gagal
+            $response = [
+                'status'  => false,
+                'message' => 'Gagal merubah sekolah asal',
+                'title'   => 'Gagal!',
+                'type'    => 'danger'
+            ];
+        }
+        return $response;
+    }
+
+    /**
+     * ============================================================
+     * PRODI PILIHAN
+     * 
+     * 1. Proses ambil data prodi dari tabel prodi berdasarkan ID akun
+     * 2. Proses update data prodi berdasarkan ID akun
+     * ============================================================
+     */
+    public function get_prodi($id)
+    {
+        $this->db->select('*')
+            ->from('pmb_prodi_pilihan a')
+            ->join('ref_jns_prodi b', 'b.id_prodi = a.id_prodi', 'LEFT')
+            ->join('ref_prodi c', 'c.kode_prodi = b.kode_prodi', 'LEFT')
+            ->where(['id_akun' => $id]);
+        return $this->db->get()->row();
+    }
+
+    public function edit_prodi($data)
+    {
+        // set data untuk diupdate
+        $value = [
+            'id_prodi'   => $data['id_prodi'],
+            'urutan'     => $this->urutan->urutan($data['id_prodi']),
+            'no_daftar'  => $this->urutan->nomordaftar($data['id_prodi']),
+            'updated_at' => date("Y-m-d H:i:s")
+        ];
+        // update tabel prodi
+        $update = $this->db->update('pmb_prodi_pilihan', $value, ['id_akun' => $data['id_akun']]);
+        // cek status simpan
+        if ($update) {
+            // buat respon berhasil
+            $response = [
+                'status'  => true,
+                'message' => 'Prodi berhasil diubah',
+                'title'   => 'Berhasil!',
+                'type'    => 'success'
+            ];
+        } else {
+            // buat respon gagal
+            $response = [
+                'status'  => false,
+                'message' => 'Gagal merubah prodi',
+                'title'   => 'Gagal!',
+                'type'    => 'danger'
+            ];
+        }
+        return $response;
+    }
+
+    // proses penerimaan pendaftar
+    // merubah status diterima pada tabel akun pendaftar
     public function status_diterima($id)
     {
         if ($id) {
@@ -361,8 +658,8 @@ class M_pendaftar extends CI_Model
     private function _get_datatables_query()
     {
         $table = "( SELECT * FROM v_data_pendaftar ) as new_tb";
-        $column_order = array('status_diterima', 'nm_pd', 'no_daftar', null, 'tgl_akun');
-        $column_search = array('nm_pd', 'no_daftar', 'jenjang', 'nm_prodi');
+        $column_order = array('status_diterima', 'nm_pd', 'no_daftar', 'nama_prodi', 'tgl_akun');
+        $column_search = array('nm_pd', 'no_daftar', 'jenjang', 'nama_prodi');
         $orders = array('tgl_akun' => 'DESC');
 
         $this->db->from($table);
