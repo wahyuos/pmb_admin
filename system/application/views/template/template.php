40e467f9c5b5653541710b794f927620d9f4f6d4
wahyuos <wahyukamaludin@outlook.com>
2020-12-15 22:23:34 +0700

    hilangkan dropdown user

--- system/application/views/template/template.php
+++ system/application/views/template/template.php
@@ -46,21 +46,13 @@
                 <div class="navbar-collapse collapse">
                     <ul class="navbar-nav navbar-align">
                         <li class="nav-item dropdown">
-                            <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-toggle="dropdown">
-                                <i class="align-middle" data-feather="settings"></i>
+                            <a class="nav-icon d-inline-block d-sm-none">
+                                <i class="align-middle" data-feather="user"></i>
                             </a>
 
-                            <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-toggle="dropdown">
+                            <a class="nav-link d-none d-sm-inline-block">
                                 <img src="<?= base_url("assets/img/logo.png") ?>" class="avatar img-fluid rounded-circle mr-1" width="100" height="100" alt="<?= $this->session->nama_user; ?>" /> <span class="text-dark"><?= $this->session->nama_user; ?></span>
                             </a>
-                            <div class="dropdown-menu dropdown-menu-right">
-                                <a class="dropdown-item" href="pages-profile.html"><i class="align-middle mr-1" data-feather="user"></i> Profile</a>
-                                <a class="dropdown-item" href="#"><i class="align-middle mr-1" data-feather="pie-chart"></i> Analytics</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="pages-settings.html">Settings & Privacy</a>
-                                <a class="dropdown-item" href="#">Help</a>
-                                <a class="dropdown-item" href="<?= base_url("logout") ?>">Sign out</a>
-                            </div>
                         </li>
                     </ul>
                 </div>
