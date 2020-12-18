888e3d81cac8b2c529e82f3a4a39030afa1d9af8
wahyuos <wahyukamaludin@outlook.com>
2020-12-18 14:17:41 +0700

    tambah lib baru

--- system/application/config/autoload.php
+++ system/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'date');
+$autoload['libraries'] = array('database', 'session', 'date', 'cek_pendaftaran');
 
 /*
 | -------------------------------------------------------------------
