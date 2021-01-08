fb540d089a81194058f406f5d428b3c9c7e73b6c
wahyuos <wahyukamaludin@outlook.com>
2021-01-08 11:39:43 +0700

    order berdasarkan tanggal daftar

--- system/application/models/M_pendaftar.php
+++ system/application/models/M_pendaftar.php
@@ -724,7 +724,7 @@ class M_pendaftar extends CI_Model
         $table = "( SELECT * FROM v_data_pendaftar WHERE tahun_akademik = '$ta' " . $by_mitra . " ) as new_tb";
         $column_order = $column;
         $column_search = array('nm_pd', 'no_daftar', 'hp_akun', 'sekolah', 'nama_prodi', 'jalur');
-        $orders = array('tgl_akun' => 'DESC');
+        $orders = array('tgl_daftar' => 'DESC');
 
         $this->db->from($table);
 
