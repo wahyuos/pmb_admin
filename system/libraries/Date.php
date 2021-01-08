d0ea64128cf26f0a9383804478098f78bdc86309
wahyuos <wahyukamaludin@outlook.com>
2021-01-08 10:10:21 +0700

    menambahkan jam pada tanggal

--- system/libraries/Date.php
+++ system/libraries/Date.php
@@ -35,22 +35,33 @@ class CI_Date
     public function tanggal($tanggal, $format = null)
     {
         if ($tanggal) {
+            // cek apakah ada jam nya
+            $tgl_jam = explode(" ", $tanggal);
+            // jika hanya tanggal
+            if (count($tgl_jam) < 2) {
+                $new_tgl = $tgl_jam[0];
+                $jam     = "";
+            } else {
+                $new_tgl = $tgl_jam[0];
+                $jam     = " " . $tgl_jam[1];
+            }
+
             // nama hari
-            $hari = $this->hari($tanggal);
+            $hari = $this->hari($new_tgl);
             // memisahkan tanggal untuk menyesuaikan posisinya
-            $pecahkan = explode('-', $tanggal);
+            $pecahkan = explode('-', $new_tgl);
             // nama bulan
-            $nm_bulan = $this->bulan($tanggal);
+            $nm_bulan = $this->bulan($new_tgl);
 
             if ($format == 's') {
                 // jika s, maka nama bulan disingkat, hanya 3 huruf pertama (tanpa hari)
-                return $pecahkan[2] . ' ' . substr($nm_bulan, 0, 3) . ' ' . $pecahkan[0];
+                return $pecahkan[2] . ' ' . substr($nm_bulan, 0, 3) . ' ' . $pecahkan[0] . $jam;
             } elseif ($format == 'p') {
                 // jika p, maka nama bulan tidak disingkat (tanpa hari)
-                return $pecahkan[2] . ' ' . $nm_bulan . ' ' . $pecahkan[0];
+                return $pecahkan[2] . ' ' . $nm_bulan . ' ' . $pecahkan[0] . $jam;
             } else {
                 // jika kosong, maka akan ditampilkan tanggal lengkap (har, tanggal bulan tahun)
-                return $hari . ', ' . $pecahkan[2] . ' ' . $nm_bulan . ' ' . $pecahkan[0];
+                return $hari . ', ' . $pecahkan[2] . ' ' . $nm_bulan . ' ' . $pecahkan[0] . $jam;
             }
         } else {
             return "Format tanggal salah";
